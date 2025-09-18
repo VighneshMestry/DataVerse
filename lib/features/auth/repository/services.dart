976a317aa9568e9a7c06c9a4dfdc741399144cf9
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2025-09-18 18:36:42 +0530

    URL update

--- lib/features/auth/repository/services.dart
+++ lib/features/auth/repository/services.dart
@@ -22,8 +22,8 @@ final servicesProvider = StateNotifierProvider<Services, bool>((ref) {
 // });
 
 class Services extends StateNotifier<bool> {
-  // String uri = "https://dataverse-po8o.onrender.com";
-  String uri = "http://localhost:4000";    // Development url
+  String uri = "https://dataverse-po8o.onrender.com";
+  // String uri = "http://localhost:4000";    // Development url
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
